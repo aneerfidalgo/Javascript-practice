930cc9520213cc5324e5b68bdf082d4ebd2678f5
Aneer Fidalgo <aneer@Aneers-MacBook-Air.local>
2022-07-20 16:38:28 -0400

    working with basic variables & operators

---
+++ yourPlayground.js
@@ -0,0 +1,29 @@
+console.log("hello world");
+console.log("Aner");
+
+//variables
+name = "Peter Pan";
+console.log(name);
+
+sentece =
+  "how are you doing today, nice to see you, hope you have a great day!";
+
+// operators
+food = "pizza";
+console.log(food);
+
+food = 20;
+console.log(food);
+
+// more operators
+fruit = prompt("what is your favorite fruit?");
+
+console.log(fruit);
+
+food = 20;
+tipPercentage = 0.2;
+tipAmount = food * tipPercentage;
+
+console.log(tipAmount);
+
+//user input
